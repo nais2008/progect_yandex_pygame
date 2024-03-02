c202e5d0b4b6f1452487ff102318e7f5b9f33900
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-03-03 01:29:46 +0500

    start_window

--- const_value.py
+++ const_value.py
@@ -1,11 +1,21 @@
 import pygame
 
 # data pygame
+pygame.font.init()
+
 SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 1080, 720
 CAPTION = "game которая заслуживает 100 баллов"
 ICON = pygame.image.load("data/img/logo.jpg")
+
+
+def get_font(font_size):
+    return pygame.font.Font("data/font/font.otf", font_size)
+
+
 FPS = 120
+BG_START = pygame.image.load("data/img/fon.png")
 
 # colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (175, 175, 175)
--- start_window.py
+++ start_window.py
@@ -1,70 +1,93 @@
-import pygame
 import os
 import sys
-import const_value
+from const_value import *
+from load_image import load_image
+from button import Button
 
 
 pygame.init()
-screen = pygame.display.set_mode((const_value.WINDOW_WIDTH, const_value.WINDOW_HEIGHT))
-pygame.display.set_caption(const_value.CAPTION)
-pygame.display.set_icon(const_value.ICON)
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption(CAPTION)
+pygame.display.set_icon(ICON)
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+# Начало игры
+def play():
+    pass
 
 
+# Правила
+def rules():
+    running = True
+
+    while running:
+        pos = pygame.mouse.get_pos()
+        screen.fill(BLACK)
+
+        rules_text = get_font(48).render("This is the RULES screen.", True, WHITE)
+        rules_rect = rules_text.get_rect(center=(640, 260))
+        screen.blit(rules_text, rules_rect)
+
+        rules_back_button = Button(image=None, pos=(640, 460),
+                                   text_input="Back", font=get_font(36),
+                                   base_color=BLACK, hover_color=GRAY)
+        rules_back_button.change_color(pos)
+        rules_back_button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if rules_back_button.check_for_input(pos):
+                    start_window()
+
+
+# Выход
+def quit_game():
+    pygame.quit()
+    sys.exit(1)
+
+
+# стартовая страница
 def start_window():
-    intro_text = ["Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('img/fon.jpg'), (const_value.WINDOW_WIDTH, const_value.WINDOW_HEIGHT + 80))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    clock = pygame.time.Clock()
-    for line in intro_text:
-        string_rendered = font.render(line, 1, const_value.BLACK)
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = const_value.WINDOW_WIDTH // 2 - 150
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
     running = True
     while running:
+        screen.blit(BG_START, (0, 0))
+
+        pos = pygame.mouse.get_pos()
+        menu_text = get_font(100).render("Меню", True, WHITE)
+        menu_rect = menu_text.get_rect(center=(590, 100))
+        play_button = Button(image=None, pos=(590, 250),
+                             text_input="Играть", font=get_font(75),
+                             base_color=pygame.color.Color("#8f8f8f"),
+                             hover_color=WHITE)
+        rules_button = Button(image=None, pos=(590, 400),
+                              text_input="Правила", font=get_font(75),
+                              base_color=pygame.color.Color("#8f8f8f"),
+                              hover_color=WHITE)
+        quit_button = Button(image=None, pos=(590, 600),
+                             text_input="Выйти", font=get_font(75),
+                             base_color=pygame.color.Color("#8f8f8f"),
+                             hover_color=WHITE)
+        screen.blit(menu_text, menu_rect)
+
+        for button in [play_button, rules_button, quit_button]:
+            button.change_color(pos)
+            button.update(screen)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                main()  # начинаем игру
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.check_for_input(pos):
+                    play()
+                if rules_button.check_for_input(pos):
+                    rules()
+                if quit_button.check_for_input(pos):
+                    pygame.quit()
+                    sys.exit()
+        pygame.display.update()
         pygame.display.flip()
-        clock.tick(const_value.FPS)
     pygame.quit()
     sys.exit()
-
-
-def main():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-    pygame.display.quit()
