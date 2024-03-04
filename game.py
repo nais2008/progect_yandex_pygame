75b227d33eaa9b88709bc958f839738df4df3e20
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-03-05 01:15:54 +0500

    meteorit

--- game.py
+++ game.py
@@ -1,10 +1,15 @@
 import sys
 from spaceship import *
+from meteorit import *
 
 
 def start_play():
     pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
     sp = Player("img/anim_sp/0.png", 20)
+    meteorits = pygame.sprite.Group()
+    meteorits.add(Meteorit("img/anim_met1/0.png", 10))
+    meteorits.add(Meteorit("img/anim_met2/6.png", 12))
+    meteorits.add(Meteorit("img/anim_met2/0.png", 8))
     running = True
     while running:
         clock = pygame.time.Clock()
@@ -15,6 +20,7 @@ def start_play():
                 sys.exit()
         sp.update()
         sp.animation()
+        meteorits.draw(screen)
         pygame.display.update()
         clock.tick(FPS)
     pygame.quit()
--- meteorit.py
+++ meteorit.py
@@ -10,6 +10,10 @@ class Meteorit(pygame.sprite.Sprite):
         self.speed = speed
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, WINDOW_WIDTH - self.rect.width)
-        self.rect.y = 0 - self.rect.height
+        self.rect.y = 0
 
-    def update(self):
+    def update(self, *args):
+        if self.rect.y < args[0] - 20:
+            self.rect.y += self.speed
+        else:
+            self.rect.y = 0 - self.rect.height
